644d947d3821ed6b8ce290819c399502e1bd4b09
Javone <javosmith@indigoempirellc.com>
2025-09-25 15:12:57 -0400

    Removed hover effect from css and added logic to change color of the tile on mouse over.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let body = document.querySelector('body')
 let sketch = document.querySelector('.sketch-container')
 
+function buildSketch(n){createColumn(n)}
 
 function createColumn(n){
     for(let x = n ; x > 0 ; x--){
@@ -16,15 +17,23 @@ function createTile(col,n){
         let tile = document.createElement('div')
         tile.setAttribute('class','tile')
         tile.addEventListener('mouseover', () =>{
-            tile.style.backgroundColor = ''
+            tile.style.backgroundColor = `${randomColor()}`
         })
         col.append(tile)
     }
 }
 
-function buildSketch(n){createColumn(n)}
+function randomColor(){
+    let alphacharacters = 'abcdef0123456789'
+    let hex = '#'
+    for(let x = 6; x > 0; x--){
+        hex += alphacharacters[Math.floor(Math.random() * 16)]
+    }
+    return hex
+}
 
-buildSketch(5)
+
+buildSketch(50)
 
 
 
