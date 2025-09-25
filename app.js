ccd79bec5856e9ac291c7aa1890f793cccfa4459
Javone <javosmith@indigoempirellc.com>
2025-09-25 12:39:29 -0400

    Changed from having two container for CSS to having a container and tiles. Changed logic to include tiles.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 let body = document.querySelector('body')
-let title = document.createElement('h1')
-title.innerText = 'Etch-A-Sketch'
+let sketch = document.querySelector('.sketch-container')
+let tile = document.createElement('div')
 
-console.log(body);
+for(let x = 10; x > 0; x--){
+    let column = document.createElement('div')
+    column.setAttribute('class','column-container')
+    for(let y = 10; y > 0; y--){
+        let tile = document.createElement('div')
+        tile.setAttribute('class','tile')
+        column.append(tile)
+    }
+    sketch.append(column)
+}
 
-body.append(title)
+
+
+
+
+
+
+
 
 
 
